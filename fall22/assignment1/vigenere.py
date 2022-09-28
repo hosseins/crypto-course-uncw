4ea7ca59fedacd96a6b411ea2dd3c28d8000b6ee
Hossein Siadati <ninja@Hosseins-MacBook-Pro.local>
2022-09-28 10:32:47 -0400

    tested encryption/decryption

--- fall22/assignment1/vigenere.py
+++ fall22/assignment1/vigenere.py
@@ -27,37 +27,54 @@ def generateKeyFile(filePath, keySize):
 def encrypt(plainText, key):
     cipher_text = ""
     key_len = len(key)
-    print("key_len:", key_len)
-
+    key_point = 0
     for i in range(len(plainText)):
         x = ord(plainText[i])
-        if plainText[i]>='A' and plainText[i]<='Z':
-            shifts = ord(key[i%key_len]) - ord('A')
-            print(shifts)
-            x = ((ord(plainText[i])-ord('A')) + ord(key[i%key_len]) - ord('A')) % 26 + ord('A')
+        shifts = ord(key[key_point % key_len]) - ord('A')
+        offset_point = ord('A') if plainText[i]>='A' and plainText[i]<='Z' else ord('a')
+        if (plainText[i]>='A' and plainText[i]<='Z') or (plainText[i]>='a' and plainText[i]<='z'):
+            key_point += 1     
+            x = ((ord(plainText[i])-offset_point) + shifts) % 26 + offset_point
         cipher_text += chr(x)
     return cipher_text
 
 def encryptFile(filePath, keyPath):
     key = readFileContent(keyPath).strip()
     plaintext = readFileContent(filePath)
-    print("file content:", plaintext)
-    print("key content:" + key + "--")
     ciphertext = encrypt(plaintext, key)
     outfilepath = filePath[:filePath.rfind(".")] + "_enc"+filePath[filePath.rfind("."):]
     writeFileContent(outfilepath, ciphertext)
     return ciphertext, outfilepath
 
-def decrypt(ciphertext, key):
-    plaintext = ""
-    return plaintext
+def decrypt(cipherText, key):
+    plain_text = ""
+    key_len = len(key)
+    key_point = 0
+    for i in range(len(cipherText)):
+        x = ord(cipherText[i])
+        shifts = ord(key[key_point % key_len]) - ord('A')
+        offset_point = ord('A') if cipherText[i]>='A' and cipherText[i]<='Z' else ord('a')
+        if (cipherText[i]>='A' and cipherText[i]<='Z') or (cipherText[i]>='a' and cipherText[i]<='z'):
+            key_point += 1     
+            x = ((ord(cipherText[i])-offset_point) - shifts) % 26 + offset_point
+        plain_text += chr(x)
+    return plain_text
+
+def decryptFile(filePath, keyPath):
+    key = readFileContent(keyPath).strip()
+    cipher_text = readFileContent(filePath)
+    plain_text = decrypt(cipher_text, key)
+    outfilepath = filePath[:filePath.rfind(".")] + "_dec"+filePath[filePath.rfind("."):]
+    writeFileContent(outfilepath, plain_text)
+    return plain_text, outfilepath
 
 def help():
     print ('''Commands are in form of [command action] [parameters]:
         keygen  keysize keypath
         encrypt keypath filepath
         decrypt keypath filepath
-    ''')    
+    ''')
+   
 print(len(sys.argv))
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -83,9 +100,16 @@ if __name__ == "__main__":
         else:
             print(sys.argv[3], sys.argv[2])
             encryptFile(sys.argv[3], sys.argv[2])
-            sys.exit(0)       
-
-    if int(sys.argv[1]) < 1:
-        print ("Keysize should be 0<")
+            sys.exit(0) 
+    elif  command_action == "decrypt":
+        if len(sys.argv) != 4:
+            help()
+            sys.exit(1)
+        else:
+            print(sys.argv[3], sys.argv[2])
+            decryptFile(sys.argv[3], sys.argv[2])
+            sys.exit(0) 
+    else:
+        print ("Invalid command.")
+        help()
         sys.exit(1)
-    print (generateKey(int(sys.argv[1])))
